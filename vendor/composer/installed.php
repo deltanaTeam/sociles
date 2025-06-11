5b58a75150f28719586e12d6dd6090213a14899c
MohamedFathy3 <codehamada39@gmail.com>
2025-06-11 15:31:25 +0300

    updateproject

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '68b863b9e1114c0be693d4783a1fe47cc9deb22f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -491,9 +491,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '68b863b9e1114c0be693d4783a1fe47cc9deb22f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
